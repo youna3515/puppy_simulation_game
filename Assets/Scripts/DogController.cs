6755b78f7162b1453f229e0cae633b3eb1086f92
raccoonD0G <w3yfrl@naver.com>
2024-05-17 08:46:32 +0900

    폴더 정리

--- Assets/Scripts/DogController.cs
+++ Assets/Scripts/DogController.cs
@@ -55,7 +55,7 @@ public class DogController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
