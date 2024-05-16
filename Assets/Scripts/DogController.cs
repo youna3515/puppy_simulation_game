8e31b3beab929ec0e0df2beecd11b53018b23073
raccoonD0G <w3yfrl@naver.com>
2024-05-17 08:03:11 +0900

    프로젝트 생성

---
+++ Assets/Scripts/DogController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogController : MonoBehaviour
+{
+    Animator _animator;
+    public enum DogState
+    {
+        Idle,
+        Walk,
+        Run,
+        DoTask,
+    }
+
+    DogState _currentState = DogState.Idle;
+    public DogState CurrentState
+    {
+        get
+        {
+            return _currentState;
+        }
+        set
+        {
+            _currentState = value;
+            switch(_currentState)
+            {
+                case DogState.Idle:
+                    _animator.Play("Idle");
+                    break;
+                case DogState.Walk:
+                    _animator.Play("Walk");
+                    break;
+                case DogState.Run:
+                    _animator.Play("Run");
+                    break;
+                case DogState.DoTask:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    void OnStartMove()
+    {
+        CurrentState = DogState.Walk;
+    }
+
+    void OnEndMove()
+    {
+        CurrentState = DogState.Idle;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
